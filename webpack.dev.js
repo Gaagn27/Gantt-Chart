704bd3afad0ab9b34076e140964af5742f9a8041
Gg <drgkng9@gmail.com>
2023-11-24 10:51:34 +0545

    webpack fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,10 +3,12 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  target: "web",
-  devServer: {
-    contentBase: "./dist",
-  },
+	mode: "development",
+	devtool: "inline-source-map",
+	target: "web",
+	devServer: {
+		static: {
+			directory: "./dist",
+		},
+	},
 });
