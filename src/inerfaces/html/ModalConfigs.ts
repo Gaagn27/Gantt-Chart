8a8bb5875ca10c49ff96172171e27719913d4ed2
Gg <drgkng9@gmail.com>
2023-11-29 16:24:25 +0545

    modal config interface

---
+++ src/inerfaces/html/ModalConfigs.ts
@@ -0,0 +1,9 @@
+import { Task } from "../Task";
+import { Input } from "./Input";
+
+type AddTask = (task: Task) => void;
+
+export interface ModalConfigs {
+	inputs: Array<Input>;
+	addTask?: AddTask;
+}
