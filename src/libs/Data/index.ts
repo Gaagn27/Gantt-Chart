36de20f4c53b966e428fb92b9492ea061aedcad8
Gg <drgkng9@gmail.com>
2023-12-20 14:57:12 +0545

    findObj fix generic

--- src/libs/Data/index.ts
+++ src/libs/Data/index.ts
@@ -1,18 +1,17 @@
-import { Task } from "../../interfaces/task/Task";
-
-export class Data{
-	public findObj<T>(obj: T, dataKey: string, dataValue: string | number | boolean): Task | false {
+export class Data {
+	public findObj<T, K>(obj: T, dataKey: string, dataValue: string | number | boolean): K | false {
 		for (const key in obj) {
-			if (Object.prototype.hasOwnProperty.call(obj, key)) {
-				const value = obj[key];
-				if (key === dataKey && value === dataValue) {
-					return obj as Task;
-				}
-				if (typeof value === "object" && value !== null) {
-					const found = this.findObj(value, dataKey, dataValue);
-					if (found) {
-						return found;
-					}
+			if (!Object.prototype.hasOwnProperty.call(obj, key)) {
+				continue;
+			}
+			const value = obj[key];
+			if (key === dataKey && value === dataValue) {
+				return obj as unknown as K;
+			}
+			if (typeof value === "object" && value !== null) {
+				const found = this.findObj(value, dataKey, dataValue);
+				if (found) {
+					return found as K;
 				}
 			}
 		}
